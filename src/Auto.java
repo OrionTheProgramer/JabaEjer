4daf738c5e73e9da6841a4830b754dc61933b110
vina <vina@VIAWSE02LC0205.duoc.acad>
2024-08-28 14:34:36 -0400

    Codigo de prueba sobre autos o algo asi la verdad no preste atencion.

---
+++ src/Auto.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Orion
+ * @version 28-08-2024
+ */
+public class Auto {
+    private String marca, modelo, color, tipoTransmision, tipoCombustible;
+    private int año, conatidadPuertas;
+    private double cilindrada;
+    
+}
